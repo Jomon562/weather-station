f8621027893b763b202ea55f6c8ce59104304ffc
Jomon562 <jomon562@gmail.com>
2026-02-21 15:33:44 -0800

    inital heartbeat commit for ESP32

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#define LED 2
+
+// put function declarations here:
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+}
