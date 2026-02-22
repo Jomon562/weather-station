6b9d48d351e0aa6d5a366f9a2b0ab1a156892c49
Jomon562 <jomon562@gmail.com>
2026-02-21 21:28:13 -0800

    Added code for polling initial data from BME280.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <Arduino.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define SDA_PIN 21
+#define SCL_PIN 22
 #define LED 2
+
+Adafruit_BME280 bme;
 
 // put function declarations here:
 
@@ -7,6 +16,18 @@ void setup() {
   // put your setup code here, to run once:
 
   pinMode(LED, OUTPUT);
+
+  Serial.begin(115200);
+  delay(1000);
+
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  if (!bme.begin(0x76)) {
+      Serial.println("Could not find BME280 sensor!");
+      while (1);
+  }
+
+  Serial.println("BME280 detected.");
 }
 
 void loop() {
@@ -14,5 +35,19 @@ void loop() {
   digitalWrite(LED, HIGH);
   delay(500);
   digitalWrite(LED, LOW);
+
+  Serial.print("Temp: ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" °C");
+
+  Serial.print("Humidity: ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
+  Serial.print("Pressure: ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.println("--------------------");
   delay(500);
 }
